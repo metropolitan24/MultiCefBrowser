71b7c063415fb3c70de44f7127b92f05523b79c4
metropolitan24 <civilmetropolitan@gmail.com>
2023-08-10 22:12:55 +0100

    Add project files.

---
+++ MultiCefBrowser/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MultiCefBrowser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TitleGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.MouseDown += delegate { DragMove(); };
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.F)
+            {
+                this.ToggleWindow();
+            }    
+        }
+
+        private void ToggleWindow()
+        {
+            switch (this.WindowState)
+            {
+                case (WindowState.Maximized):
+                    {
+                        this.WindowState = WindowState.Normal;
+                        this.BorderThickness = new System.Windows.Thickness(0);
+                        ToolbarBorder.CornerRadius = new CornerRadius(0, 0, 10, 10);
+                    }
+                    break;
+
+                default:
+                    {
+                        this.WindowState = WindowState.Maximized;
+                        this.BorderThickness = new System.Windows.Thickness(8);
+                    }
+                    break;
+            }
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+        }
+    }
+}
